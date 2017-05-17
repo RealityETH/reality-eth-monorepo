7c354d52689784a4150937aa7f5c5300a3c32c67
Edmund Edgar <ed@socialminds.jp>
2017-05-17 17:33:38 +0900

    Nuke the whole truffle build, we everything except truffle migrate with browserify

--- packages/dapp/truffle/truffle.js
+++ packages/dapp/truffle/truffle.js
@@ -1,33 +1,4 @@
-var DefaultBuilder = require("truffle-default-builder");
-
 module.exports = {
-  build: new DefaultBuilder({
-    "index.html": "index.html",
-    "index.js": [
-      "javascripts/index.js"
-    ],
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "ask.js": [
-      "javascripts/ask.js"
-    ],
-    "answer.js": [
-      "javascripts/answer.js"
-    ],
-    "question.js": [
-      "javascripts/question.js"
-    ],
-    "yours.js": [
-      "javascripts/yours.js"
-    ],
-    "jquery-3.1.1.min.js": [
-      "javascripts/jquery-3.1.1.min.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-  }),
   networks: {
     development: {
       host: "localhost",
