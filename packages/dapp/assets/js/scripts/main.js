f9c84d6c12eecb07575268bb3874a6563e36f7cd
Edmund Edgar <ed@realitykeys.com>
2017-08-23 16:57:54 +0900

    Replace multihashes dependency with ghetto ipfs prefix matching code, hoping that makes the thing work on windows

--- packages/dapp/assets/js/scripts/main.js
+++ packages/dapp/assets/js/scripts/main.js
@@ -9,7 +9,6 @@ var arb_json = require('../../../truffle/build/contracts/Arbitrator.json');
 var ipfsAPI = require('ipfs-api')
 var ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'})
 
-var multihash = require('multihashes');
 var bs58 = require('bs58');
 
 var contract = require("truffle-contract");
@@ -109,19 +108,18 @@ const monthList = [
 ];
 
 function ipfsHashToBytes32(ipfs_hash) {
-    var h = multihash.decode(bs58.decode(ipfs_hash));
-    //console.log(h);
-    if (h.name != 'sha2-256') {
-        //console.log('unexpected name', h.name);
+    var h = bs58.decode(ipfs_hash).toString('hex').replace(/^1220/, '');
+    if (h.length != 64) {
+        console.log('invalid ipfs format', ipfs_hash, h);
         return null;
     }
-    return '0x' + h.digest.toString('hex');
+    return '0x' + h;
 }
 
 function bytes32ToIPFSHash(hash_hex) {
     //console.log('bytes32ToIPFSHash starts with hash_buffer', hash_hex.replace(/^0x/, ''));
-    var buf = new Buffer(hash_hex.replace(/^0x/, ''), 'hex')
-    return bs58.encode(multihash.encode(buf, 'sha2-256'))
+    var buf = new Buffer(hash_hex.replace(/^0x/, '1220'), 'hex')
+    return bs58.encode(buf)
 }
 
 function formatForAnswer(answer, qtype) {
