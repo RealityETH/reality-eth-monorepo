09583eeb3c49edc8f9e8396ae7ca12fa6437c72b
Edmund Edgar <ed@socialminds.jp>
2017-09-01 19:13:30 +0900

    More claim tests

--- packages/dapp/truffle/contracts/test.py
+++ packages/dapp/truffle/contracts/test.py
@@ -272,9 +272,8 @@ class TestRealityCheck(TestCase):
         self.assertEqual(self.rc0.balanceOf(keys.privtoaddr(t.k4)), 1000)
         return
 
-    #@unittest.skipIf(WORKING_ONLY, "Not under construction")
+    @unittest.skipIf(WORKING_ONLY, "Not under construction")
     def test_bond_claim_arbitration_existing_final(self):
-        return
         st = None
         st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1001, "", 0, 2, t.k4)
         st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1002, "", 2, 4, t.k3)
@@ -290,7 +289,29 @@ class TestRealityCheck(TestCase):
         self.arb0.submitAnswerByArbitrator(self.rc0.address, self.question_id, to_answer_for_contract(1001), keys.privtoaddr(t.k4), to_question_for_contract(("my evidence")), startgas=200000) 
 
         self.rc0.claimWinnings(self.question_id, st['hash'], st['addr'], st['bond'], st['answer'], startgas=400000)
+        self.assertEqual(self.rc0.balanceOf(keys.privtoaddr(t.k4)), 16+8+4+2+1000)
+
+    @unittest.skipIf(WORKING_ONLY, "Not under construction")
+    def test_bond_claim_arbitration_existing_not_final(self):
+        st = None
+        st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1001, "", 0, 2, t.k4)
+        st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1002, "", 2, 4, t.k3)
+        st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1002, "", 4, 8, t.k3)
+        st = self.submitAnswerReturnUpdatedState( st, self.question_id, 1001, "", 8, 16, t.k4)
+
+        self.rc0.requestArbitration(self.question_id, value=self.arb0.getFee(), startgas=200000)
+
+        st['hash'].insert(0, self.rc0.questions(self.question_id)[QINDEX_HISTORY_HASH])
+        st['addr'].insert(0, keys.privtoaddr(t.k3))
+        st['bond'].insert(0, 0)
+        st['answer'].insert(0, to_answer_for_contract(1002))
+        self.arb0.submitAnswerByArbitrator(self.rc0.address, self.question_id, to_answer_for_contract(1002), keys.privtoaddr(t.k3), to_question_for_contract(("my evidence")), startgas=200000) 
+
+        self.rc0.claimWinnings(self.question_id, st['hash'], st['addr'], st['bond'], st['answer'], startgas=400000)
         self.assertEqual(self.rc0.balanceOf(keys.privtoaddr(t.k3)), 16+8+4+2+1000)
+
+
+
 
 
 
