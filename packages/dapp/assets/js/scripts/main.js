8dcbc621384c295e62991de1da014cc0556c7eb7
Edmund Edgar <ed@realitykeys.com>
2017-10-11 23:58:47 +0000

    Drop the max_show down a bit as too many answered questions float up too high

--- packages/dapp/assets/js/scripts/main.js
+++ packages/dapp/assets/js/scripts/main.js
@@ -120,10 +120,10 @@ var account;
 var rc;
 
 var display_entries = {
-    'questions-latest':       {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 10},
-    'questions-resolved':     {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 10},
-    'questions-closing-soon': {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 10},
-    'questions-high-reward':  {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 10}
+    'questions-latest':       {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 6},
+    'questions-resolved':     {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 6},
+    'questions-closing-soon': {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 6},
+    'questions-high-reward':  {'ids': [], 'vals': [], 'max_store': 50, 'max_show': 6}
 }
 
 // data for question detail window
