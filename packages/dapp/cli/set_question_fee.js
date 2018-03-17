0b55ec222432cec78c2c107a7c1da3bfb042d947
Edmund Edgar <ed@realitykeys.com>
2018-03-17 04:08:43 +0000

    Use the right param

--- packages/dapp/cli/set_question_fee.js
+++ packages/dapp/cli/set_question_fee.js
@@ -2,7 +2,7 @@ const rc_common = require('./rc_common.js');
 
 rc_common.checkArgumentLength(5, 'Usage: node set_question_fee.js <nonce> <gas_price_in_gwei> <fee_in_gwei>');
 
-const fee = parseInt(process.argv[3]);
+const fee = parseInt(process.argv[4]);
 if (fee == 0) throw 'Fee should be greater than 0';
 
 const params = rc_common.commonParams(process.argv);
