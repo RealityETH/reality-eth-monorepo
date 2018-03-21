08355e2d8ca648ef7f60aa7664dfe9e6b28f2b5b
Edmund Edgar <ed@realitykeys.com>
2018-03-21 07:25:02 +0000

    Redeploy test, manually add live network addresses

--- packages/dapp/truffle/truffle.js
+++ packages/dapp/truffle/truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "4"
+      network_id: "4",
+      gasPrice: 2000000000,
+      gas: 3000000
     }
   },
   solc: {
