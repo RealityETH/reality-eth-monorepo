486c141eb861f838c48d6bc04fd000fe13f5b3f6
Edmund Edgar <ed@realitykeys.com>
2018-06-23 06:43:24 +0000

    Simple command-line django app to index realitycheck questions in a database

---
+++ packages/python-services/resources/systemd/unit-status-mail.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#MAILTO="edmund.edgar@gmail.com"
+MAILTO="ed"
+MAILFROM="unit-status-mailer"
+UNIT=$1
+
+EXTRA=""
+for e in "${@:2}"; do
+  EXTRA+="$e"$'\n'
+done
+
+UNITSTATUS=$(systemctl status $UNIT)
+
+sendmail $MAILTO <<EOF
+From:$MAILFROM
+To:$MAILTO
+Subject:Status mail for unit: $UNIT
+
+Status report for unit: $UNIT
+$EXTRA
+
+$UNITSTATUS
+EOF
+
+echo -e "Status mail sent to: $MAILTO for unit: $UNIT"
---
+++ packages/python-services/src/rcpy/rcpy/settings.py
@@ -0,0 +1,69 @@
+RKETH_ENVIRONMENT = 'prod'
+import os
+
+import sys
+import rcpy.secrets as SECRETS
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRETS.SECRET_KEY
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'rcindex',
+]
+
+MIDDLEWARE = []
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rcindex',
+        'USER': 'rcindexer',
+        'PASSWORD': SECRETS.DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+RC_ADDRESS = '0xb4e1367c077856a65a738a66662601223e88b3b4'
+GETH_RPC_HOST = '127.0.0.1'
+GETH_RPC_PORT = '8545'
+GETH_TESTNET = False
+
+
