6a8ec60623bbc84c5a39eb6103463657e629b158
Edmund Edgar <ed@realitykeys.com>
2018-06-29 08:10:15 +0000

    Go to the bigger unit at smaller fractions

--- packages/python-services/src/rcpy/rcindex/eth_util.py
+++ packages/python-services/src/rcpy/rcindex/eth_util.py
@@ -5,10 +5,10 @@ def HumanReadableWei(amt_hex):
     unit = ''
     displ = ''
     amt = HexToPythonInt(amt_hex)
-    if (amt > Web3.toWei(0.01, 'ether')): 
+    if (amt > Web3.toWei(0.001, 'ether')): 
         unit = 'ether';
         displ = 'ETH';
-    elif (amt > Web3.toWei(0.01, 'gwei')):
+    elif (amt > Web3.toWei(0.001, 'gwei')):
         unit = 'gwei';
         displ = 'Gwei';
     else: 
