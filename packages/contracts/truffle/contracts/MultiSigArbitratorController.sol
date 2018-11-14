3b51c8e7a683f00d7d0c6a4ff05b2d9b2b781413
ed <Edmund Edgar>
2018-11-14 15:31:11 +0900

    Multisig arbitration contracts for arbitration consortiums, based on the Gnosis MultiSigWallet

---
+++ packages/contracts/truffle/contracts/MultiSigArbitratorController.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.25;
+
+// Import a standard Gnosis multisig wallet
+import './MultiSigWallet.sol';
+import './Arbitrator.sol';
+
+/*
+The arbitrator contract is managed by an owner address.
+
+This contract implements a MultiSig wallet to allow that owner address to be controlled by multiple parties.
+
+For most functions of the arbitrator contract, changes will require m of n parties to approve.
+
+However, we want you to be able to withdraw your own funds without the permission of the other participants.
+We therefore implement a separate Splitter Wallet to handle splitting the funds between the participants.
+
+The address of the splitter wallet is controlled by m of n, like other multi-sig wallet features.
+As well as a function to control this, we implement functions callable by anyone to:
+    * Download any outstanding funds in a realitio contract to the arbitrator contract
+    * Withdraw funds from the multisig to a splitter wallet
+*/
+
+contract MultiSigArbitratorController is MultiSigWallet {
+
+    address splitter_wallet;
+
+    /// @notice Call the withdraw function of the specified arbitrator contract to pull funds from Realitio to it.
+    /// @param arbitrator The arbitrator contract that should pull funds from Realitio to itself
+    /// @dev Arbitrator.callWithdraw() is limited to its owner (this), but it's the arbitrator's money so in fact it doesn't matter who calls it.
+    function callWithdraw(address arbitrator) 
+    external {
+        Arbitrator(arbitrator).callWithdraw();
+    }
+
+    /// @notice Withdraw money from the arbitrator contract to our splitter wallet
+    /// @param arbitrator The arbitrator contract to pull funds from
+    function withdrawToSplit(address arbitrator)
+    external {
+        Arbitrator(arbitrator).withdraw(splitter_wallet);
+    }
+
+    /// @notice Change the address of our splitter wallet
+    /// @param addr The address of the new splitter wallet
+    /// @dev 
+    function updateSplitterWallet(address addr) 
+        onlyWallet
+        validRequirement(owners.length + 1, required)
+    public {
+        splitter_wallet = addr;
+    }
+
+}
---
+++ packages/contracts/truffle/contracts/SplitBalanceHolder.sol
@@ -0,0 +1,109 @@
+pragma solidity ^0.4.25;
+
+import './Owned.sol';
+import './RealitioSafeMath256.sol';
+
+contract SplitBalanceHolder is Owned {
+
+    using RealitioSafeMath256 for uint256;
+
+    mapping(address => uint256) public balanceOf;
+    
+    // Sum of all balances in balanceOf
+    uint256 public balanceTotal; 
+
+    // List of recipients. May contain duplicates to get paid twice.
+    address[] public recipients;
+
+    event LogWithdraw(
+        address indexed user,
+        uint256 amount
+    );
+
+    function _recipientIndex(address addr) 
+        internal
+    view returns (uint256) 
+    {
+        uint256 i;
+        for(i=0; i<recipients.length; i++) {
+            if (recipients[i] == addr) {
+                return i;
+            }
+        }
+        revert("Recipient not found");
+    }
+
+    /// @notice Add a recipient to the list
+    /// @param addr The address to add
+    function addRecipient(address addr) 
+        onlyOwner
+    external {
+        recipients.push(addr);
+    }
+
+    /// @notice Remove a recipient from the list
+    /// @param old_addr The address to remove
+    function removeRecipient(address old_addr) 
+        onlyOwner
+    external {
+
+        uint256 idx = _recipientIndex(old_addr);
+        assert(recipients[idx] == old_addr);
+
+        // If you're not deleting the last item, copy the last item over to the thing you're deleting
+        uint256 last_idx = recipients.length - 1;
+        if (idx != last_idx) {
+            recipients[idx] = recipients[last_idx];
+        }
+
+        recipients.length--;
+    }
+
+    /// @notice Replace your own address with a different one
+    /// @param new_addr The new address
+    function replaceSelf(address new_addr) 
+    external {
+        uint256 idx = _recipientIndex(msg.sender);
+        assert(recipients[idx] == msg.sender);
+        recipients[idx] = new_addr;
+    }
+
+    /// @notice Allocate any unallocated funds from the contract balance
+    /// @dev Any time the contract gets funds, they will appear as unallocated
+    /// @dev Assign them to the current recipients, and mark them as allocated
+    function allocate()
+    external {
+        uint256 unallocated = address(this).balance.sub(balanceTotal);
+        require(unallocated > 0);
+
+        uint256 num_recipients = recipients.length;
+
+        // NB Rounding may leave some funds unallocated, we can claim them later
+        uint256 each = unallocated / num_recipients;
+        require(each > 0);
+
+        uint256 i;
+        for (i=0; i<num_recipients; i++) {
+            address recip = recipients[i];
+            balanceOf[recip] = balanceOf[recip].add(each);
+            balanceTotal = balanceTotal.add(each);
+        }
+
+    }
+
+    /// @notice Withdraw the address balance to the owner account
+    function withdraw() 
+    external {
+        uint256 bal = balanceOf[msg.sender];
+        require(bal > 0, "Balance must be positive");
+        balanceTotal = balanceTotal.sub(bal);
+        balanceOf[msg.sender] = 0;
+        msg.sender.transfer(bal);
+        emit LogWithdraw(msg.sender, bal);
+    }
+
+    function()
+    external payable {
+    }
+
+}
