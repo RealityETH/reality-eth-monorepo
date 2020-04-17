d009ecc9308fc5c56f60bdf74332e9332c5a40aa
Edmund Edgar <ed@realitykeys.com>
2020-04-17 06:04:48 +0000

    Remove dead code

--- packages/dapp/truffle/contracts/ExplodingCallbackClient.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.6;
-
-contract ExplodingCallbackClient {
-
-    function __realitycheck_callback(bytes32 question_id, bytes32 question_answer) {
-        throw;
-    }
-
-}
--- packages/dapp/truffle/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var RealityCheck = artifacts.require("./RealityCheck.sol");
-var Arbitrator = artifacts.require("./Arbitrator.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(RealityCheck, {overwrite: false});
-  deployer.deploy(Arbitrator);
-};
