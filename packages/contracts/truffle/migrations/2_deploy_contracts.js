3bd84a8acd5c18204b7532476efccf55f9520754
Edmund Edgar <ed@socialminds.jp>
2020-09-19 14:31:42 +0900

    Delete the truffle migrations, as we no longer use truffle for migrating things

--- packages/contracts/truffle/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Realitio = artifacts.require("./Realitio.sol");
-var Arbitrator = artifacts.require("./Arbitrator.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Realitio);
-  deployer.deploy(Arbitrator);
-};
