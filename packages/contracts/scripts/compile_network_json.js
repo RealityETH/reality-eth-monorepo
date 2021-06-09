fe6b330ed17cdc8f12bf5736126ae15c29005ffc
Edmund Edgar <ed@socialminds.jp>
2021-06-09 01:53:34 +0000

    compile network script, uses require-glob, name json files .json to make require happy

---
+++ packages/contracts/scripts/compile_network_json.js
@@ -0,0 +1,26 @@
+const requireGlob = require('require-glob');
+const fs = require('fs');
+const path = require('path');
+
+var SEPARATOR_PATTERN = /[\\\/]/;
+
+function toCombinedValues(a, b) {
+        return a.concat(b);
+}
+
+function toSplitPath(filePath) {
+        return filePath.split(SEPARATOR_PATTERN);
+}
+
+function keygen(options, fileObj) {
+        var uniquePath = fileObj.path.replace(fileObj.base, '');
+        var parsedPath = path.parse(uniquePath);
+
+        return [parsedPath.dir, parsedPath.name]
+                .map(toSplitPath)
+                .reduce(toCombinedValues)
+                .filter(Boolean);
+}
+
+const all_config = requireGlob.sync('./../networks/*/*/*.json', {"keygen": keygen});
+fs.writeFileSync(__dirname + '/../generated/networks.json', JSON.stringify(all_config, null, 4));
