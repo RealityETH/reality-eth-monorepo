8328f0e0fef88898676a7ac5078142643c739909
Edmund Edgar <ed@socialminds.jp>
2021-06-15 15:16:09 +0900

    Supply the network name

--- packages/graph/render-templates.js
+++ packages/graph/render-templates.js
@@ -3,12 +3,15 @@ const mustache = require('mustache');
 const rc_contracts = require('@reality.eth/contracts');
 
 const net_id = process.argv[2];
-console.log('net_id', net_id);
+//console.log('net_id', net_id);
 const config = rc_contracts.realityETHConfig(net_id, 'ETH');
-console.log('config', config);
+//console.log('config', config);
 
+const net_config = rc_contracts.networkData(net_id);
+const network = net_config.network_name;
 
 const templateData = {
+    "network": network,
     "address": config.address,
     "block": config.block
 }
