614bea17dda7a031806d6282abd38a479396a6a2
Edmund Edgar <ed@socialminds.jp>
2021-06-18 15:46:17 +0900

    Specify a contract instance for each token deployed on the network

--- packages/graph/render-templates.js
+++ packages/graph/render-templates.js
@@ -5,21 +5,33 @@ const rc_contracts = require('@reality.eth/contracts');
 const net_id = process.argv[2];
 //console.log('net_id', net_id);
 
-const token = rc_contracts.defaultTokenForNetwork(net_id);
-const config = rc_contracts.realityETHConfig(net_id, token);
-console.log('config', config);
-
 const net_config = rc_contracts.networkData(net_id);
 const network = net_config.network_name;
 
-const templateData = {
-    "network": network,
-    "address": config.address,
-    "block": config.block
-}
+const token_list = rc_contracts.networkTokenList(net_id)
 
 const template = fs.readFileSync('subgraph.template.yaml').toString();
+const ds_template = fs.readFileSync('subgraph.datasource.template.yaml').toString();
+
+let dses = [];
+
+for(var token in token_list) {
+    const config = rc_contracts.realityETHConfig(net_id, token);
+    const dsData = {
+        "network": network,
+        "address": config.address,
+        "block": config.block,
+        "token": token
+    }
+    const ds_section = mustache.render(ds_template, dsData);
+    dses.push(ds_section);
+}
+
+const templateData = {
+    "datasources": dses.join("\n")
+}
+
 fs.writeFileSync(
     'subgraph.yaml', 
-    mustache.render(template, templateData),
+    mustache.render(template, templateData)
 );
