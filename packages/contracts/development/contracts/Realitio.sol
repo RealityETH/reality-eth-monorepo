9db17e8d7f7bc621fc108a54a8ca054e8ab095ff
Edmund Edgar <ed@socialminds.jp>
2021-07-17 04:57:31 +0000

    (untested) ability to set a minimum bond

--- packages/contracts/development/contracts/Realitio.sol
+++ packages/contracts/development/contracts/Realitio.sol
@@ -45,7 +45,8 @@ contract Realitio is BalanceHolder {
         uint32 timeout,
         uint32 opening_ts,
         uint256 nonce,
-        uint256 created
+        uint256 created,
+        uint256 min_bond
     );
 
     event LogFundAnswerBounty(
@@ -101,6 +102,7 @@ contract Realitio is BalanceHolder {
         bytes32 best_answer;
         bytes32 history_hash;
         uint256 bond;
+        uint256 min_bond;
     }
 
     // Stored in a mapping indexed by commitment_id, a hash of commitment hash, question, bond. 
@@ -169,9 +171,14 @@ contract Realitio is BalanceHolder {
         _;
     }
 
-    modifier bondMustDouble(bytes32 question_id) {
+    modifier bondMustDoubleAndExceedMinimum(bytes32 question_id) {
         require(msg.value > 0, "bond must be positive"); 
-        require(msg.value >= (questions[question_id].bond.mul(2)), "bond must be double at least previous bond");
+        uint256 current_bond = questions[question_id].bond;
+        if (current_bond == 0) {
+            require(msg.value >= (questions[question_id].min_bond), "bond must exceed the minimum");
+        } else {
+            require(msg.value >= (current_bond.mul(2)), "bond must be double at least previous bond");
+        }
         _;
     }
 
@@ -254,15 +261,40 @@ contract Realitio is BalanceHolder {
         require(templates[template_id] > 0, "template must exist");
 
         bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
-        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, msg.sender, nonce));
+        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, msg.sender, nonce, uint256(0)));
 
-        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts);
-        emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, now);
+        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts, 0);
+        emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, now, 0);
 
         return question_id;
     }
 
-    function _askQuestion(bytes32 question_id, bytes32 content_hash, address arbitrator, uint32 timeout, uint32 opening_ts) 
+    /// @notice Ask a new question, specifying a minimum bond, and return the ID
+    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
+    /// @param template_id The ID number of the template the question will use
+    /// @param question A string containing the parameters that will be passed into the template to make the question
+    /// @param arbitrator The arbitration contract that will have the final word on the answer if there is a dispute
+    /// @param timeout How long the contract should wait after the answer is changed before finalizing on that answer
+    /// @param opening_ts If set, the earliest time it should be possible to answer the question.
+    /// @param nonce A user-specified nonce used in the question ID. Change it to repeat a question.
+    /// @param min_bond The minimum bond that should be allowed for the first answer.
+    /// @return The ID of the newly-created question, created deterministically.
+    function askQuestionWithMinBond(uint256 template_id, string question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce, uint256 min_bond) 
+        // stateNotCreated is enforced by the internal _askQuestion
+    public payable returns (bytes32) {
+
+        require(templates[template_id] > 0, "template must exist");
+
+        bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
+        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, msg.sender, nonce, min_bond));
+
+        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts, 0);
+        emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, now, min_bond);
+
+        return question_id;
+    }
+
+    function _askQuestion(bytes32 question_id, bytes32 content_hash, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 min_bond) 
         stateNotCreated(question_id)
     internal {
 
@@ -290,6 +322,7 @@ contract Realitio is BalanceHolder {
         questions[question_id].opening_ts = opening_ts;
         questions[question_id].timeout = timeout;
         questions[question_id].bounty = bounty;
+        questions[question_id].min_bond = min_bond;
 
     }
 
@@ -311,7 +344,7 @@ contract Realitio is BalanceHolder {
     /// @param max_previous If specified, reverts if a bond higher than this was submitted after you sent your transaction.
     function submitAnswer(bytes32 question_id, bytes32 answer, uint256 max_previous) 
         stateOpen(question_id)
-        bondMustDouble(question_id)
+        bondMustDoubleAndExceedMinimum(question_id)
         previousBondMustNotBeatMaxPrevious(question_id, max_previous)
     external payable {
         _addAnswerToHistory(question_id, answer, msg.sender, msg.value, false);
@@ -341,7 +374,7 @@ contract Realitio is BalanceHolder {
     /// @dev Specifying the answerer is useful if you want to delegate the commit-and-reveal to a third-party.
     function submitAnswerCommitment(bytes32 question_id, bytes32 answer_hash, uint256 max_previous, address _answerer) 
         stateOpen(question_id)
-        bondMustDouble(question_id)
+        bondMustDoubleAndExceedMinimum(question_id)
         previousBondMustNotBeatMaxPrevious(question_id, max_previous)
     external payable {
 
