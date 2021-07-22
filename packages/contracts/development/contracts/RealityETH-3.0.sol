48af6f33b85f71141cad4fd45b3f73d2ae7b512f
Edmund Edgar <ed@socialminds.jp>
2021-07-22 00:58:34 +0000

    Add min_bond, fix existing tests to use the new hash and gas amounts

--- packages/contracts/development/contracts/RealityETH-3.0.sol
+++ packages/contracts/development/contracts/RealityETH-3.0.sol
@@ -47,6 +47,11 @@ contract RealityETH_v3_0 is BalanceHolder {
         uint256 created
     );
 
+    event LogMinimumBond(
+        bytes32 indexed question_id,
+        uint256 min_bond
+    );
+
     event LogFundAnswerBounty(
         bytes32 indexed question_id,
         uint256 bounty_added,
@@ -104,6 +109,7 @@ contract RealityETH_v3_0 is BalanceHolder {
         bytes32 best_answer;
         bytes32 history_hash;
         uint256 bond;
+        uint256 min_bond;
     }
 
     // Stored in a mapping indexed by commitment_id, a hash of commitment hash, question, bond. 
@@ -172,9 +178,14 @@ contract RealityETH_v3_0 is BalanceHolder {
         _;
     }
 
-    modifier bondMustDouble(bytes32 question_id) {
+    modifier bondMustDoubleAndMatchMinimum(bytes32 question_id) {
         require(msg.value > 0, "bond must be positive"); 
-        require(msg.value >= (questions[question_id].bond * 2), "bond must be double at least previous bond");
+        uint256 current_bond = questions[question_id].bond;
+        if (current_bond == 0) {
+            require(msg.value >= (questions[question_id].min_bond), "bond must exceed the minimum");
+        } else {
+            require(msg.value >= (current_bond * 2), "bond must be double at least previous bond");
+        }
         _;
     }
 
@@ -256,15 +267,43 @@ contract RealityETH_v3_0 is BalanceHolder {
         require(templates[template_id] > 0, "template must exist");
 
         bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
-        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, address(this), msg.sender, nonce));
+        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, uint256(0), address(this), msg.sender, nonce));
 
-        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts);
+        // We emit this event here because _askQuestion doesn't need to know the unhashed question. Other events are emitted by _askQuestion.
         emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, block.timestamp);
+        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts, 0);
 
         return question_id;
     }
 
-    function _askQuestion(bytes32 question_id, bytes32 content_hash, address arbitrator, uint32 timeout, uint32 opening_ts) 
+    /// @notice Ask a new question and return the ID
+    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
+    /// @param template_id The ID number of the template the question will use
+    /// @param question A string containing the parameters that will be passed into the template to make the question
+    /// @param arbitrator The arbitration contract that will have the final word on the answer if there is a dispute
+    /// @param timeout How long the contract should wait after the answer is changed before finalizing on that answer
+    /// @param opening_ts If set, the earliest time it should be possible to answer the question.
+    /// @param nonce A user-specified nonce used in the question ID. Change it to repeat a question.
+    /// @param min_bond The minimum bond that may be used for an answer.
+    /// @return The ID of the newly-created question, created deterministically.
+    function askQuestionWithMinBond(uint256 template_id, string memory question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce, uint256 min_bond) 
+        // stateNotCreated is enforced by the internal _askQuestion
+    public payable returns (bytes32) {
+
+        require(templates[template_id] > 0, "template must exist");
+
+        bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
+        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, min_bond, address(this), msg.sender, nonce));
+
+        // We emit this event here because _askQuestion doesn't need to know the unhashed question.
+        // Other events are emitted by _askQuestion.
+        emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, block.timestamp);
+        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts, min_bond);
+
+        return question_id;
+    }
+
+    function _askQuestion(bytes32 question_id, bytes32 content_hash, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 min_bond) 
         stateNotCreated(question_id)
     internal {
 
@@ -292,6 +331,11 @@ contract RealityETH_v3_0 is BalanceHolder {
         questions[question_id].timeout = timeout;
         questions[question_id].bounty = bounty;
 
+        if (min_bond > 0) {
+            questions[question_id].min_bond = min_bond;
+            emit LogMinimumBond(question_id, min_bond);
+        }
+
     }
 
     /// @notice Add funds to the bounty for a question
@@ -312,7 +356,7 @@ contract RealityETH_v3_0 is BalanceHolder {
     /// @param max_previous If specified, reverts if a bond higher than this was submitted after you sent your transaction.
     function submitAnswer(bytes32 question_id, bytes32 answer, uint256 max_previous) 
         stateOpen(question_id)
-        bondMustDouble(question_id)
+        bondMustDoubleAndMatchMinimum(question_id)
         previousBondMustNotBeatMaxPrevious(question_id, max_previous)
     external payable {
         _addAnswerToHistory(question_id, answer, msg.sender, msg.value, false);
@@ -328,7 +372,7 @@ contract RealityETH_v3_0 is BalanceHolder {
     /// @param answerer The account to which the answer should be credited
     function submitAnswerFor(bytes32 question_id, bytes32 answer, uint256 max_previous, address answerer)
         stateOpen(question_id)
-        bondMustDouble(question_id)
+        bondMustDoubleAndMatchMinimum(question_id)
         previousBondMustNotBeatMaxPrevious(question_id, max_previous)
     external payable {
         require(answerer != NULL_ADDRESS, "answerer must be non-zero");
@@ -359,7 +403,7 @@ contract RealityETH_v3_0 is BalanceHolder {
     /// @dev Specifying the answerer is useful if you want to delegate the commit-and-reveal to a third-party.
     function submitAnswerCommitment(bytes32 question_id, bytes32 answer_hash, uint256 max_previous, address _answerer) 
         stateOpen(question_id)
-        bondMustDouble(question_id)
+        bondMustDoubleAndMatchMinimum(question_id)
         previousBondMustNotBeatMaxPrevious(question_id, max_previous)
     external payable {
 
--- packages/contracts/tests/python/test.py
+++ packages/contracts/tests/python/test.py
@@ -68,9 +68,9 @@ def calculate_commitment_id(question_id, answer_hash, bond):
 def calculate_content_hash(template_id, question_str, opening_ts):
     return "0x"+encode_hex(bytes(Web3.solidityKeccak(['uint256', 'uint32', 'string'], [template_id, opening_ts, question_str])))
 
-def calculate_question_id(cntrct, template_id, question_str, arbitrator, timeout, opening_ts, nonce, sender):
+def calculate_question_id(cntrct, template_id, question_str, arbitrator, timeout, opening_ts, nonce, sender, min_bond):
     content_hash = calculate_content_hash(template_id, question_str, opening_ts)
-    return "0x"+encode_hex(bytes(Web3.solidityKeccak(['bytes32', 'address', 'uint32', 'address', 'address', 'uint256'], [content_hash, arbitrator, timeout, cntrct, sender, nonce])))
+    return "0x"+encode_hex(bytes(Web3.solidityKeccak(['bytes32', 'address', 'uint32', 'uint256', 'address', 'address', 'uint256'], [content_hash, arbitrator, timeout, min_bond, cntrct, sender, nonce])))
 
 def calculate_history_hash(last_history_hash, answer_or_commitment_id, bond, answerer, is_commitment):
     return "0x"+encode_hex(bytes(Web3.solidityKeccak(['bytes32', 'bytes32', 'uint256', 'address', 'bool'], [last_history_hash, answer_or_commitment_id, bond, answerer, is_commitment])))
@@ -190,7 +190,7 @@ class TestRealitio(TestCase):
 
         txid = self.arb0.functions.setQuestionFee(100).transact(self.standard_tx)
 
-        expected_question_id = calculate_question_id(self.rc0.address, 0, "my question", self.arb0.address, 30, 0, 0, self.web3.eth.accounts[0])
+        expected_question_id = calculate_question_id(self.rc0.address, 0, "my question", self.arb0.address, 30, 0, 0, self.web3.eth.accounts[0], 0)
         
         txid = self.rc0.functions.askQuestion(
             0,
@@ -1353,7 +1353,7 @@ class TestRealitio(TestCase):
         txid = self.rc0.functions.submitAnswer(self.question_id, to_answer_for_contract(12345), 0).transact(self._txargs(val=1))
         rcpt = self.web3.eth.getTransactionReceipt(txid)
 
-        self.assertTrue(rcpt['cumulativeGasUsed'] < 102000)
+        self.assertTrue(rcpt['cumulativeGasUsed'] < 103000)
 
         # NB The second answer should be cheaper than the first.
         # This is what we want, because you may need to be able to get a challenge through at busy times
@@ -1432,7 +1432,7 @@ class TestRealitio(TestCase):
             0,
             0
         ).transact(self._txargs(val=126, sender=k4))
-        question_id = calculate_question_id(self.rc0.address, 0, "my question 2", k5, 10, 0, 0, k4)
+        question_id = calculate_question_id(self.rc0.address, 0, "my question 2", k5, 10, 0, 0, k4, 0)
 
         bounty = self.rc0.functions.questions(question_id).call()[QINDEX_BOUNTY]
         self.assertEqual(bounty, 126-123, "The bounty is what's left after the question fee is deducted")
@@ -1445,7 +1445,7 @@ class TestRealitio(TestCase):
             0,
             0
         ).transact(self._txargs(val=122, sender=k5))
-        question_id = calculate_question_id(self.rc0.address, 0, "my question 3", k5, 10, 0, 0, k5)
+        question_id = calculate_question_id(self.rc0.address, 0, "my question 3", k5, 10, 0, 0, k5, 0)
 
         bounty = self.rc0.functions.questions(question_id).call()[QINDEX_BOUNTY]
         self.assertEqual(bounty, 122, "The arbitrator isn't charged their fee, so their whole payment goes to the bounty")
