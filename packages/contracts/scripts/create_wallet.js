17abcdab3b0a7f3f65997d681174543d8fd5acc0
Edmund Edgar <ed@socialminds.jp>
2021-09-08 01:20:25 +0000

    Script to create a new private key and/or output its address

---
+++ packages/contracts/scripts/create_wallet.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const ethers = require('ethers');
+
+const out = process.argv[2]
+if (!out) {
+    throw new Error("Usage: node create_wallet.js <outfile>");
+}
+
+let w;
+if (fs.existsSync(out)) {
+    const k = fs.readFileSync(out, 'utf8').replace(/\n/, '')
+    w = new ethers.Wallet(k)
+    console.log("Wallet already exists at " + out + ", delete it if you want to create a new one");
+} else { 
+    w = ethers.Wallet.createRandom();
+    fs.writeFileSync(out, w.signingKey.keyPair.privateKey);
+}
+
+console.log(w.signingKey.address);
