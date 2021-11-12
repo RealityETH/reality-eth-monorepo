a38a0cf1da20d39297afe27cee8428929128d39a
Edmund Edgar <ed@socialminds.jp>
2021-11-12 05:17:50 +0000

    Nuke the python services, as the twitter bot is now handled with JS/Graph in packages/twitter-bot

--- packages/python-services/resources/systemd/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#cp ../bin/ipfs /usr/local/bin/ipfs
-cp unit-status-mail.sh /usr/local/bin/unit-status-mail.sh
-cp unit-status-mail@.service /etc/systemd/system/unit-status-mail@.service
-cp geth.service /lib/systemd/system/geth.service
-cp rcpyd.service /lib/systemd/system/rcpyd.service
-# cp geth-ropsten.service /lib/systemd/system/geth-ropsten.service
-# cp ipfs.service /lib/systemd/system/ipfs.service
--- packages/python-services/src/rcpy/rctwitter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RctwitterConfig(AppConfig):
-    name = 'rctwitter'
