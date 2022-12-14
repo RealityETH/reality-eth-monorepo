df752f6e74d1e9c86dacfbe1fec6077ded503a70
Edmund Edgar <ed@socialminds.jp>
2022-12-14 01:29:45 +0000

    Enable sending out to an arbitrary address, fix some ethers incompatibility

--- packages/contracts/scripts/migrate_wallet.js
+++ packages/contracts/scripts/migrate_wallet.js
@@ -16,29 +16,39 @@ if (!rpc) {
     throw new Error("Usage: node migrate_wallet.js <from> <to> <rpc>");
 }
 
+const is_out = (process.argv.length > 4 && process.argv[5] == 'out')
+
 // For infura we just pass the network name eg kovan
 const provider = (rpc.startsWith('http')) ? new ethers.providers.JsonRpcProvider(rpc) : new ethers.providers.InfuraProvider(rpc);
 
 const fpriv = fs.readFileSync(from, 'utf8').replace(/\n/, '')
-const tpriv = fs.readFileSync(to, 'utf8').replace(/\n/, '')
-
 const f = new ethers.Wallet(fpriv, provider);
-const t = new ethers.Wallet(tpriv, provider);
+
+// to can be an address if we specify out, otherwise expect a key
+let to_addr;
+if (is_out) {
+    to_addr = to;
+} else {
+    const tpriv = fs.readFileSync(to, 'utf8').replace(/\n/, '')
+    const t = new ethers.Wallet(tpriv, provider);
+    to_addr = t.address;
+}
+console.log(to_addr);
 
 //console.log(provider);
-migrateFunds(provider, f, t);
+migrateFunds(provider, f, to_addr);
 
-async function migrateFunds(provider, f, t) {
+async function migrateFunds(provider, f, to_addr) {
 
-    const bal = await provider.getBalance(f.signingKey.address)
+    const bal = await provider.getBalance(f.address)
     const gas_price = await provider.getGasPrice()
-    const nonce = await provider.getTransactionCount(f.signingKey.address, 'latest')
+    const nonce = await provider.getTransactionCount(f.address, 'latest')
     const gas_limit = 21000;
 
     // console.log(f)
 
     const txdata = {
-      to: t.signingKey.address,
+      to: to_addr,
       value: bal.sub((gas_price.mul(gas_limit))),
       nonce: nonce,
       gasLimit: ethers.utils.hexlify(gas_limit), 
