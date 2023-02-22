c286427910e7eec74010fa0d4d5461b9d5bb9172
Edmund Edgar <ed@socialminds.jp>
2023-02-22 04:35:33 +0000

    Update contracts to latest to deploy after regenesis

--- packages/contracts/non_standard_deployments/zksync2/hardhat.config.ts
+++ packages/contracts/non_standard_deployments/zksync2/hardhat.config.ts
@@ -3,19 +3,20 @@ import "@matterlabs/hardhat-zksync-solc";
 
 module.exports = {
   zksolc: {
-    version: "1.2.2",
+    version: "1.3.1",
     compilerSource: "binary",
     settings: {},
   },
-  defaultNetwork: "zkTestnet",
+  defaultNetwork: "zkSyncTestnet",
+
   networks: {
-    zkTestnet: {
-      url: "https://zksync2-testnet.zksync.dev", // URL of the zkSync network RPC
-      ethNetwork: "goerli", // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
     },
   },
   solidity: {
-    version: "0.8.16",
+    version: "0.8.17",
   },
 };
