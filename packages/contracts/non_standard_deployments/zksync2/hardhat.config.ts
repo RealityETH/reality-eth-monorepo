6a98c847b28fd9a817a35ba020b22cc4c8d24acd
Edmund Edgar <ed@socialminds.jp>
2023-03-08 01:44:16 +0000

    preparing for the zksync alpha

--- packages/contracts/non_standard_deployments/zksync2/hardhat.config.ts
+++ packages/contracts/non_standard_deployments/zksync2/hardhat.config.ts
@@ -15,6 +15,11 @@ module.exports = {
       ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
     },
+    zkSyncMainnet: {
+      url: "https://zksync2-mainnet.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+    },
   },
   solidity: {
     version: "0.8.17",
