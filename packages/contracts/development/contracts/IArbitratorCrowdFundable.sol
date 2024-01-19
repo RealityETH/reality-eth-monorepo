58f75625798c578244685c862cb3a988ed745e8b
Edmund Edgar <ed@socialminds.jp>
2024-01-19 15:20:06 +0900

    Allow function name with underscores for legacy reasons (linting, no bytecode change)

--- packages/contracts/development/contracts/IArbitratorCrowdFundable.sol
+++ packages/contracts/development/contracts/IArbitratorCrowdFundable.sol
@@ -3,5 +3,6 @@
 pragma solidity 0.8.20;
 
 interface IArbitratorCrowdFundable {
+    /* solhint-disable func-name-mixedcase */
     function arbitration_bounties(bytes32) external view returns (uint256);
 }
