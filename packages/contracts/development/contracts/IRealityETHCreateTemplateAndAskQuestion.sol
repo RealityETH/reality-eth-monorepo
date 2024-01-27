3c2d7c08f80a8dae87d741e1608198edaee82493
Edmund Edgar <ed@socialminds.jp>
2024-01-27 14:23:13 +0900

    Move createTemplateAndAskQuestion out of the core interface prior to removing it (no bytecode change)

---
+++ packages/contracts/development/contracts/IRealityETHCreateTemplateAndAskQuestion.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity 0.8.20;
+
+// This function was removed from IRealityETH in version 4.
+interface IRealityETHCreateTemplateAndAskQuestion {
+    function createTemplateAndAskQuestion(string calldata content, string calldata question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce) external payable returns (bytes32);
+}
