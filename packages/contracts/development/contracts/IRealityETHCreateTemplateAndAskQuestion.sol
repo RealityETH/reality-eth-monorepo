d48c4d3b5b989b0dcce13a3b845b282fc1a51a88
Edmund Edgar <ed@socialminds.jp>
2024-01-27 14:29:22 +0900

    Remove createTemplateAndAskQuestion as nobody was using it (#107) (changes bytecode)

--- packages/contracts/development/contracts/IRealityETHCreateTemplateAndAskQuestion.sol
+++ packages/contracts/development/contracts/IRealityETHCreateTemplateAndAskQuestion.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.20;
 
-// This function was removed from IRealityETH in version 4.
+// This function was removed from reality.eth in version 4.
 interface IRealityETHCreateTemplateAndAskQuestion {
     function createTemplateAndAskQuestion(string calldata content, string calldata question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce) external payable returns (bytes32);
 }
