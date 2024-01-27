9e41f11157bc67941579788738672abeb781eaf5
Edmund Edgar <ed@socialminds.jp>
2024-01-27 14:29:22 +0900

    Remove createTemplateAndAskQuestion as nobody was using it (#107) (changes bytecode)

--- packages/contracts/development/contracts/RealityETH-4.0.sol
+++ packages/contracts/development/contracts/RealityETH-4.0.sol
@@ -121,34 +121,6 @@ contract RealityETH_v4_0 is BalanceHolder, IRealityETHCore, IRealityETHHistoryVe
         return id;
     }
 
-    /// @notice Create a new reusable template and use it to ask a question
-    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
-    /// @param content The template content
-    /// @param question A string containing the parameters that will be passed into the template to make the question
-    /// @param arbitrator The arbitration contract that will have the final word on the answer if there is a dispute
-    /// @param timeout How long the contract should wait after the answer is changed before finalizing on that answer
-    /// @param opening_ts If set, the earliest time it should be possible to answer the question.
-    /// @param nonce A user-specified nonce used in the question ID. Change it to repeat a question.
-    /// @return The ID of the newly-created template, which is created sequentially.
-    function createTemplateAndAskQuestion(
-        string memory content,
-        string memory question,
-        address arbitrator,
-        uint32 timeout,
-        uint32 opening_ts,
-        uint256 nonce
-    )
-        public
-        payable
-        returns (
-            // stateNotCreated is enforced by the internal _askQuestion
-            bytes32
-        )
-    {
-        uint256 template_id = createTemplate(content);
-        return askQuestion(template_id, question, arbitrator, timeout, opening_ts, nonce);
-    }
-
     /// @notice Ask a new question and return the ID
     /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
     /// @param template_id The ID number of the template the question will use
